c1097b81f0e1d987e7093e8389d1f90785426098
Vicky Kumar <imvickykumar999@gmail.com>
2025-08-08 14:22:44 +0530

    media upload scheduled successfully

--- vixpost/socialmedia/admin.py
+++ vixpost/socialmedia/admin.py
@@ -1,10 +1,26 @@
-# admin.py
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import ScheduledTask
 
 @admin.register(ScheduledTask)
 class ScheduledTaskAdmin(admin.ModelAdmin):
-    list_display = ('name', 'run_at', 'executed')
-    list_filter = ('executed',)
-    search_fields = ('name',)
-    ordering = ('-run_at',)
+    list_display = ('id', 'name', 'run_at', 'executed', 'media_preview')
+    readonly_fields = ('media_preview',)
+
+    def media_preview(self, obj):
+        if obj.media:
+            file_url = obj.media.url
+            if obj.media.name.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp')):
+                return format_html('<img src="{}" width="100" height="100" style="object-fit:contain;" />', file_url)
+            elif obj.media.name.lower().endswith(('.mp4', '.webm', '.ogg')):
+                return format_html('''
+                    <video width="200" height="150" controls>
+                        <source src="{}" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                ''', file_url)
+            else:
+                return format_html('<a href="{}" target="_blank">Download File</a>', file_url)
+        return "-"
+    
+    media_preview.short_description = 'Media Preview'
--- vixpost/socialmedia/models.py
+++ vixpost/socialmedia/models.py
@@ -1,10 +1,18 @@
-# models.py
 from django.db import models
 
 class ScheduledTask(models.Model):
     name = models.CharField(max_length=100)
     run_at = models.DateTimeField()
     executed = models.BooleanField(default=False)
+    
+    # New fields
+    caption = models.TextField(blank=True, null=True)
+    media = models.FileField(
+        upload_to='scheduled_media/', 
+        blank=True, 
+        null=True,
+        help_text="Upload image/video/gif"
+    )
 
     def __str__(self):
         return self.name
